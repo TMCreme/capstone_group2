ebe721b326820afd13aecf71e9800bacdb4c2f2c
unknown <tonnybright123@yahoo.com>
2020-07-15 10:53:18 +0000

    restructure download, plot returns, ljung-box test

--- README.md
+++ README.md
@@ -1,5 +1,18 @@
 # capstone_group2
 
-This is WorldQuant University Capstone Project 
-Project Title : Statistical Properties of Stock Market Data
+## This is WorldQuant University Capstone Project 
+## Project Title : Statistical Properties of Stock Market Data
 
+
+Data duration is from June 2006 to May 2020
+
+
+Market Indices used are : the S&P 500 (representing the developed market),the JSE.JO (representing South Africa emerging market) and the 000001.SSShanghai Stock Exchange Composite Index (representing China emerging market)
+
+
+
+* Download the data
+* Calulate returns 
+* Describe the basic statistics of the data
+* Plot the returns 
+* Ljung Box test for auto-correlation
--- main.py
+++ main.py
@@ -1,16 +1,47 @@
 import pandas as pd 
 import yfinance as yf
 import matplotlib.pyplot as plt 
+import statsmodels.tsa.stattools as ts
+import statsmodels.api as sm
+from statsmodels.stats.diagnostic import acorr_ljungbox
+import numpy as np
 import math
 
 
-start_date = '2008-01-01'
-end_date = '2019-12-31'
-country_data = yf.Ticker('^JN0U.JO')
-df = country_data.history(start=start_date, end=end_date)
-# df['Close'].plot(title="TSLA's stock price")
-print(df.head())
+start_date = '2006-06-01'
+end_date = '2020-05-31'
 
-data_brazil = yf.Ticker('^bvsp')
-df1 = data_brazil.history(start=start_date, end=end_date)
-print(df1.head())
+joburg_df = yf.download('JSE.JO', start=start_date, end=end_date)
+snp500_df = yf.download('^GSPC', start=start_date, end=end_date)
+shanghai_df = yf.download('000001.SS', start=start_date, end=end_date)
+
+# Calculating returns 
+joburg_df["Returns"] = joburg_df["Adj Close"].pct_change()
+snp500_df["Returns"] = snp500_df["Adj Close"].pct_change()
+shanghai_df["Returns"] = shanghai_df["Adj Close"].pct_change()
+
+# Describing the basic statistics of the data
+print(joburg_df.describe())
+
+## Plotting the returns data 
+fig = plt.figure()
+ax1 = fig.add_axes([0.1,0.1,0.8,0.8])
+ax1.plot(joburg_df["Returns"])
+ax1.plot(snp500_df["Returns"])
+ax1.plot(shanghai_df["Returns"])
+ax1.set_xlabel("Date")
+ax1.set_ylabel("Returns")
+ax1.set_title("Stock Index daily returns for the dataset")
+plt.show()
+
+# dw_stat = ts.durbin_watson(joburg_df["Returns"])
+
+# LJung Box Test for auto correlation on the JSE.JO
+joburg_df.dropna(subset = ["Returns"], inplace=True)
+res = sm.tsa.ARMA(joburg_df["Returns"], (1,1)).fit(disp=-1)
+lbox_test = acorr_ljungbox(res.resid,lags=2, return_df=True)
+
+print(lbox_test)
+
+
+
